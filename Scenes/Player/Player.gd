1e423520b4cd97a77cdddceec934cad476a42c89
hei hei OK OK! <98307235+laisammy@users.noreply.github.com>
2026-04-07 19:49:41 +0100

    Added cloud assets and player into godot

---
+++ Scenes/Player/Player.gd
@@ -0,0 +1,14 @@
+extends CharacterBody3D
+
+class_name Player
+
+const GRAVITY: float = 40.0
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+func _physics_process(delta: float) -> void:
+	velocity.y -= GRAVITY * delta
+	move_and_slide()
